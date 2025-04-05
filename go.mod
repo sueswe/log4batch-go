874827a5c1909fb6da989d3b0a5da6dfcac1dd7e
sueswe <nookie.suess@gmail.com>
2025-04-05 16:22:05 +0200

    First version of a log4batch implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module omstp/log4batch
+
+go 1.24.1
---
+++ log4batch.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	infoLog := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+	warningLog := log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime)
+	dPart := flag.String("d", "INFO|ERROR|WARNING", "Message-type.")
+	flag.Parse()
+
+	MESSAGE := os.Args[1:]
+	myMessage := strings.Join(MESSAGE[1:], " ")
+
+	if *dPart == "INFO" {
+		newMessage, _ := strings.CutPrefix(myMessage, "INFO")
+		infoLog.Println(newMessage)
+	} else if *dPart == "WARN" {
+		newMessage, _ := strings.CutPrefix(myMessage, "WARN")
+		warningLog.Println(newMessage)
+	} else if *dPart == "ERROR" {
+		newMessage, _ := strings.CutPrefix(myMessage, "ERROR")
+		errorLog.Println(newMessage)
+	} else {
+		//newMessage, _ := strings.CutPrefix(myMessage, "INFO")
+		fmt.Println(MESSAGE)
+	}
+}
